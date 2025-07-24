d3ddb4cda7e369d12f8ba636a01506ddf1f2bd37
Pratik Yesane <pratiky2000@gmail.com>
2025-07-25 01:34:49 +0530

    Change incident resolution endpoint from GET to PATCH, enhance error handling for invalid incident IDs, and improve response formatting.

--- src/app/api/incidents/[id]/resolve/route.ts
+++ src/app/api/incidents/[id]/resolve/route.ts
@@ -1,21 +1,35 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { prisma } from '../../../../../../prisma/config/prismaClient';
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "../../../../../../prisma/config/prismaClient";
 
-export async function GET(
-    request: NextRequest,
-    { params }: { params: Promise<{ id: string }> }
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
 ) {
-    const incidentId = await params;
+  const incidentId = await params;
 
+  if (!incidentId) {
+    return NextResponse.json({ error: "Invalid incident ID" }, { status: 400 });
+  }
+
+  try {
     const incident = await prisma.incident.update({
-        where: { id: incidentId.id },
-        data: {
-            resolved: true,
-        },
+      where: { id: incidentId.id },
+      data: {
+        resolved: true,
+      },
     });
-
-    return new NextResponse(JSON.stringify(incident), {
+    return NextResponse.json(
+      `Incident Resolved of ${
+        incident.type
+      } for ${incident.date.toLocaleDateString()}`,
+      {
         status: 200,
-        headers: { 'Content-Type': 'application/json' },
-    });
+      }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { error: `Failed to update incident` },
+      { status: 500 }
+    );
+  }
 }
