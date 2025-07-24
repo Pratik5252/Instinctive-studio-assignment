f691e9edc1da177d18c2767b57978f296f6435ea
Pratik Yesane <pratiky2000@gmail.com>
2025-07-25 01:34:27 +0530

    Refactor incident retrieval logic to include camera data, improve error handling, and standardize code formatting.

--- src/app/api/incidents/route.ts
+++ src/app/api/incidents/route.ts
@@ -1,26 +1,28 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { prisma } from '../../../../prisma/config/prismaClient';
+import { NextRequest, NextResponse } from "next/server";
+import { prisma } from "../../../../prisma/config/prismaClient";
 
 export async function GET(request: NextRequest) {
-    const nextUrl = request.nextUrl;
-    console.log(nextUrl);
+  const nextUrl = request.nextUrl;
+  const resolvedParams = nextUrl.searchParams.get("resolve");
 
-    const resolvedParams = nextUrl.searchParams.get('resolve');
+  const resolved =
+    resolvedParams === "true"
+      ? true
+      : resolvedParams === "false"
+      ? false
+      : undefined;
 
-    const resolved =
-        resolvedParams === 'true'
-            ? true
-            : resolvedParams === 'false'
-            ? false
-            : undefined;
-
+  try {
     const incidents = await prisma.incident.findMany({
-        where: { resolved },
-        orderBy: { tsStart: 'desc' },
+      where: { resolved },
+      orderBy: { tsStart: "desc" },
+      include: { Camera: true },
     });
-
-    return new NextResponse(JSON.stringify(incidents), {
-        status: 200,
-        headers: { 'Content-Type': 'application/json' },
-    });
+    return NextResponse.json(incidents, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to fetch incidents" },
+      { status: 500 }
+    );
+  }
 }
