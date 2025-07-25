89c6e40dbb3b3d1583e939264de7941b416ddb08
Pratik Yesane <pratiky2000@gmail.com>
2025-07-25 17:11:48 +0530

    Refactor API endpoints to use environment variable for base URL, add getCameras function for camera data retrieval, and enhance error handling for fetch requests.

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,14 +1,26 @@
 import { Incident, Camera, Prisma } from "@prisma/client";
 
+const APP_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
 export type IncidentWithCamera = Prisma.IncidentGetPayload<{
   include: { Camera: true };
 }>;
 
+export const getCameras = async (): Promise<Camera[]> => {
+  const response = await fetch(`${APP_URL}/api/camera`, {
+    method: "GET",
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch cameras");
+  }
+  return response.json();
+};
+
 export const getIncidents = async (): Promise<IncidentWithCamera[]> => {
-  const response = await fetch(
-    `http://localhost:3000/api/incidents?resolve=false`,
-    { method: "GET" }
-  );
+  const response = await fetch(`${APP_URL}/api/incidents?resolve=false`, {
+    method: "GET",
+  });
 
   if (!response.ok) {
     throw new Error("Failed to fetch incidents");
@@ -18,7 +30,7 @@ export const getIncidents = async (): Promise<IncidentWithCamera[]> => {
 
 export const resolveIncident = async (incidentId: string) => {
   const response = await fetch(
-    `http://localhost:3000/api/incidents/${incidentId}/resolve`,
+    `${APP_URL}/api/incidents/${incidentId}/resolve`,
     { method: "PATCH" }
   );
 
